cd4c4ee2a5dff0d4a99f9cf073a8bd775a5c8403
trarck <trarck@gmail.com>
2013-12-29 20:06:52 +0800

    [all]加入所技能的文件的头文件

--- yhge/yhge.h
+++ yhge/yhge.h
@@ -3,7 +3,30 @@
 
 #include "YHGEMacros.h"
 
+//Arithmetic
+#include "Arithmetic/Search/AStar/AstarNode.h"
+#include "Arithmetic/Search/AStar/Astar.h"
+#include "Arithmetic/Search/AStar/AstarFlexible.h"
+#include "Arithmetic/Search/AStar/CallbackAstar.h"
+
+//CocosExt
+#include "CocosExt/Actions/CCParabolaMove.h"
+#include "CocosExt/CCCallFuncNO.h"
+#include "CocosExt/CCGeometryValue.h"
+#include "CocosExt/CCSelectorWrap.h"
+
 //message
 #include "message.h"
+//event
+#include "Event/Event.h"
+#include "Event/UIEvent.h"
+#include "Event/EventHandle.h"
+#include "Event/EventHandleWrap.h"
+#include "Event/EventTarget.h"
+#include "Event/EventListenerManager.h"
+
+//math
+#include "Math/MersenneTwister.h"
+#include "Math/RandomMT.h"
 
 #endif //COCOS_YHGE_H_
