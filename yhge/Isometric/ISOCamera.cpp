9f5756e0bfc1d97cf2bf03d9d71de376d3af87e2
trarck <trarck@gmail.com>
2014-01-14 14:49:00 +0800

    完善缩放

--- yhge/Isometric/ISOCamera.cpp
+++ yhge/Isometric/ISOCamera.cpp
@@ -6,7 +6,12 @@ NS_CC_YHGE_BEGIN
 
 ISOCamera::ISOCamera()
 :m_tWorldPosition(CCPointZero)
-,m_scale(1.0f)
+,m_bSmoothMove(NULL)
+,m_scaleX(1.0f)
+,m_scaleY(1.0f)
+,m_delegate(NULL)
+,m_lastScaleX(1.0f)
+,m_lastScaleY(1.0f)
 {
     
 }
@@ -27,8 +32,8 @@ bool ISOCamera::init()
  */
 void  ISOCamera::move(float deltaX,float deltaY)
 {
-    m_tWorldPosition.x-=deltaX;
-    m_tWorldPosition.y-=deltaY;
+    m_tWorldPosition.x+=deltaX;
+    m_tWorldPosition.y+=deltaY;
 
     this->updatePosition();
 }
@@ -39,8 +44,8 @@ void  ISOCamera::move(float deltaX,float deltaY)
  */
 void  ISOCamera::moveTo(float x,float y)
 {
-    m_tWorldPosition.x=-x;
-    m_tWorldPosition.y=-y;
+    m_tWorldPosition.x=x;
+    m_tWorldPosition.y=y;
 
     this->updatePosition();
 
@@ -53,8 +58,8 @@ void  ISOCamera::moveTo(float x,float y)
  */
 void  ISOCamera::moveOpposite(float deltaX,float deltaY)
 {
-    m_tWorldPosition.x+=deltaX;
-    m_tWorldPosition.y+=deltaY;
+    m_tWorldPosition.x-=deltaX;
+    m_tWorldPosition.y-=deltaY;
 
     this->updatePosition();
 }
@@ -64,26 +69,89 @@ void  ISOCamera::moveOpposite(float deltaX,float deltaY)
  */
 void ISOCamera::updatePosition()
 {
-    if (m_moveDelegate) {
-        m_moveDelegate->onCameraMove(m_tWorldPosition);
+    if (m_delegate) {
+        m_delegate->onCameraMove(m_tWorldPosition);
     }
 //    m_pGameWorld->setPosition(m_tWorldPosition);
 //    m_pGameWorld->updateMapPosition(ccp(-m_tWorldPosition.x,-m_tWorldPosition.y));
 }
 
 /**
- * 取得坐标所在game的位置
+ * 缩放
  */
-CCPoint  ISOCamera::getLocationInWorld(const CCPoint& position)
+void ISOCamera::scaleBy(float scale)
 {
-    float x=position.x-m_tWorldPosition.x;
-    float y=position.y-m_tWorldPosition.y;
+    scaleBy(scale,scale);
+}
 
-    x/=m_scale;
-    y/=m_scale;
+/**
+ * 缩放
+ */
+void ISOCamera::scaleBy(float scaleX,float scaleY)
+{
+    m_lastScaleX=m_scaleX;
+    m_lastScaleY=m_scaleY;
+    
+    m_scaleX+=scaleX;
+    m_scaleY+=scaleY;
+
+    this->updateScale();
+}
+
+/**
+ * 缩放
+ */
+void ISOCamera::scaleTo(float scale)
+{
+    scaleTo(scale, scale);
+}
+
+/**
+ * 缩放
+ */
+void ISOCamera::scaleTo(float scaleX,float scaleY)
+{
+    m_lastScaleX=m_scaleX;
+    m_lastScaleY=m_scaleY;
+    
+    m_scaleX=scaleX;
+    m_scaleY=scaleY;
+    
+    this->updateScale();
+}
+
+/**
+ * 更新缩放
+ */
+void ISOCamera::updateScale()
+{
+    //update position.新的缩放模式下的位置
+    CCAssert(m_lastScaleX!=0 && m_lastScaleY!=0, "ISOCamera::updateScale neg is zero");
+    
+    m_tWorldPosition.x=m_tWorldPosition.x*m_scaleX/m_lastScaleY;
+    m_tWorldPosition.y=m_tWorldPosition.y*m_scaleY/m_lastScaleY;
+    
+    if (m_delegate) {
+        m_delegate->onCameraScale(m_scaleX,m_scaleY);
+        m_delegate->onCameraMove(m_tWorldPosition);
+    }
+    
+}
+
+/**
+ * 取得屏幕坐标所在game的位置
+ */
+CCPoint ISOCamera::getLocationInWorld(const CCPoint& position)
+{
+    CCAssert(m_scaleX!=0 && m_scaleY!=0, "ISOCamera::updateScale neg is zero");
+    
+    float x=m_tWorldPosition.x+position.x;
+    float y=m_tWorldPosition.y+position.y;
+
+    x/=m_scaleX;
+    y/=m_scaleY;
 
     return ccp(x,y);
 }
-
 
 NS_CC_YHGE_END
--- yhge/Isometric/ISOCamera.h
+++ yhge/Isometric/ISOCamera.h
@@ -6,22 +6,42 @@
 
 NS_CC_YHGE_BEGIN
 
-class ISOCameraMoveDelegate
+class ISOCameraDelegate
 {
 public:
     
     /**
-     * 相机移动调用
+     * 相机移动
+     * worldPosition是相机所在的世界坐标
      */
     virtual void onCameraMove(const CCPoint& worldPosition)=0;
     
     /**
-     * 相机移动结束调用
+     * 相机移动结束
+     * worldPosition是相机所在的世界坐标
      */
     virtual void onCameraMoveEnd(const CCPoint& worldPosition){};
     
+    /**
+     * 相机缩放
+     * scaleX,scaleY是相机的缩放值
+     */
+    virtual void onCameraScale(float scaleX,float scaleY){};
+    
+    /**
+     * 相机缩放结束
+     * scaleX,scaleY是相机的缩放值
+     */
+    virtual void onCameraScaleEnd(float scaleX,float scaleY){};
+    
 };
 
+
+/**
+ * 相机
+ * 分为二种：1.不处理缩放，缩放交给游戏世界处理
+ *         2.处理绽放，并需要游戏世界配合。
+ */
 class ISOCamera: public CCObject
 {
 public:
@@ -33,19 +53,20 @@ public:
 
     /**
      * 移动
-     * 相对位置
+     * 相对位置，屏幕坐标系的值。
      */
 	void move(float deltaX,float deltaY);
     
     /**
      * 移动
-     * 绝对位置
+     * 绝对位置，屏幕坐标系的值。
      */
     void moveTo(float x,float y);
 
     /**
      * 反向移动
      * 相对位置
+     * 移动相机，其实在反向移动游戏世界
      */
 	void moveOpposite(float deltaX,float deltaY);
     
@@ -53,9 +74,34 @@ public:
      * 更新坐标
      */
     void updatePosition();
+    
+    /**
+     * 缩放
+     */
+    void scaleBy(float scale);
+    
+    /**
+     * 缩放
+     */
+    void scaleBy(float scaleX,float scaleY);
+    
+    /**
+     * 缩放
+     */
+    void scaleTo(float scale);
+    
+    /**
+     * 缩放
+     */
+    void scaleTo(float scaleX,float scaleY);
+    
+    /**
+     * 更新缩放
+     */
+    void updateScale();
 
     /**
-     * 取得坐标所在game的位置
+     * 取得屏幕坐标所在game的位置
      */
     CCPoint getLocationInWorld(const CCPoint& position);
 
@@ -69,24 +115,60 @@ public:
         return m_bSmoothMove;
     }
     
-    inline void setMoveDelegate(ISOCameraMoveDelegate* moveDelegate)
+    inline void setMoveDelegate(ISOCameraDelegate* moveDelegate)
     {
-        m_moveDelegate = moveDelegate;
+        m_delegate = moveDelegate;
     }
     
-    inline ISOCameraMoveDelegate* getMoveDelegate()
+    inline ISOCameraDelegate* getMoveDelegate()
     {
-        return m_moveDelegate;
+        return m_delegate;
     }
-
-    void setScale(float scale)
+    
+    inline float getScale()
     {
-        m_scale = scale;
+        CCAssert(m_scaleX==m_scaleX, "m_scaleX and m_scaleX is not the same");
+        return m_scaleX;
     }
-
-    float getScale()
+    
+    inline void setScaleX(float scaleX)
     {
-        return m_scale;
+        m_scaleX = scaleX;
+    }
+    
+    inline float getScaleX()
+    {
+        return m_scaleX;
+    }
+    
+    inline void setScaleY(float scaleY)
+    {
+        m_scaleY = scaleY;
+    }
+    
+    inline float getScaleY()
+    {
+        return m_scaleY;
+    }
+    
+    inline void setLastScaleX(float lastScaleX)
+    {
+        m_lastScaleX = lastScaleX;
+    }
+    
+    inline float getLastScaleX()
+    {
+        return m_lastScaleX;
+    }
+    
+    inline void setLastScaleY(float lastScaleY)
+    {
+        m_lastScaleY = lastScaleY;
+    }
+    
+    inline float getLastScaleY()
+    {
+        return m_lastScaleY;
     }
 
 protected:
@@ -97,6 +179,7 @@ protected:
     
     /**
      * 相机所在游戏世界位置
+     * 移动相机，其实在反向移动游戏世界.所以在设置游戏世界位置时，坐标值都取反
      */
     CCPoint m_tWorldPosition;
     
@@ -104,12 +187,20 @@ protected:
      * 相机移动事件代理
      * weak reference
      */
-    ISOCameraMoveDelegate* m_moveDelegate;
+    ISOCameraDelegate* m_delegate;
 
     /**
-     * 相机的缩放
+     * 相机的缩放参数
+     * 相机本身是不能缩放，只能对观察的世界进行缩放。和世界的缩放值保持一致。
+     * 相机放大，game world在放大。
+     * 相机缩小，game world在缩小。
      */
-    float m_scale;
+    float m_scaleX;
+    float m_scaleY;
+    
+    //旧的缩放
+    float m_lastScaleX;
+    float m_lastScaleY;
 };
 
 NS_CC_YHGE_END
