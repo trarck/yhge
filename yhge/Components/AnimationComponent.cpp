26fd44dbeb40afe571a70cc95b9145e9be1e990a
trarck <trarck@gmail.com>
2014-01-15 14:28:32 +0800

    重构动画组件

--- yhge/Components/AnimationComponent.cpp
+++ yhge/Components/AnimationComponent.cpp
@@ -1,25 +1,29 @@
 #include "AnimationComponent.h"
-#include <yhge/Message/MessageManager.h>
+#include <yhge/message.h>
+#include "ComponentMessageDefine.h"
 
 USING_NS_CC;
 
 NS_CC_YHGE_BEGIN
 
+static const int kEightDirectionAction=1001;
+
 AnimationComponent::AnimationComponent()
 :m_animations(NULL)
+,m_lastAnimation(NULL)
 ,m_lastAction(NULL)
 {
 	CCLOG("AnimationComponent create");
+	m_name="AnimationComponent";
 }
 
 AnimationComponent::~AnimationComponent()
 {
 	CCLOG("AnimationComponent destroy");
 
-	CC_SAFE_RELEASE(m_animations);
-    CC_SAFE_RELEASE(m_lastAction);
+	CC_SAFE_RELEASE_NULL(m_animations);
+    CC_SAFE_RELEASE_NULL(m_lastAction);
 }
-
 
 bool AnimationComponent::init()
 {
@@ -39,67 +43,146 @@ bool AnimationComponent::init(CCDictionary* data)
 	return true;
 }
 
-CCObject* AnimationComponent::animationForName(const std::string& name)
+bool AnimationComponent::registerMessages()
 {
-	return m_animations->objectForKey(name);
+    CCLOG("AnimationComponent::registerMessages");
+    
+    MessageManager::defaultManager()->registerReceiver(m_owner,MSG_CHANGE_ANIMATION, NULL ,message_selector(AnimationComponent::onChangeAnimation));
+    
+    return true;
 }
 
-CCAction* AnimationComponent::actionForName(const std::string& name)
+
+void AnimationComponent::cleanupMessages()
 {
-	return (CCAction*) m_animations->objectForKey(name);
+	CCLOG("AnimationComponent::cleanupMessages");
+    MessageManager::defaultManager()->removeReceiver(m_owner,MSG_CHANGE_ANIMATION);
 }
 
-CCAction* AnimationComponent::actionForName(const std::string& name ,int index)
+
+/**
+ * 按名称取得动画
+ */
+CCAnimation* AnimationComponent::animationForName(const std::string& name)
 {
-	return (CCAction*)((CCArray*)(m_animations->objectForKey(name)))->objectAtIndex(index);
+	return (CCAnimation*) m_animations->objectForKey(name);
 }
 
-CCAction* AnimationComponent::actionForName(const std::string& name ,const std::string& key)
+CCAnimation* AnimationComponent::animationForName(const std::string& name ,int index)
 {
-	return (CCAction*)((CCDictionary*)(m_animations->objectForKey(name)))->objectForKey(key);
+	return (CCAnimation*)((CCArray*)(m_animations->objectForKey(name)))->objectAtIndex(index);
 }
 
-CCArray* AnimationComponent::actionListForName(const std::string& name)
+CCAnimation* AnimationComponent::animationForName(const std::string& name ,const std::string& key)
+{
+	return (CCAnimation*)((CCDictionary*)(m_animations->objectForKey(name)))->objectForKey(key);
+}
+
+/**
+ * 按名称取得动画列表
+ */
+CCArray* AnimationComponent::animationListForName(const std::string& name)
 {
 	return (CCArray*)(m_animations->objectForKey(name));
 }
 
-CCDictionary* AnimationComponent::actionMapForName(const std::string& name)
+/**
+ * 按名称取得动画字典
+ */
+CCDictionary* AnimationComponent::animationMapForName(const std::string& name)
 {
 	return (CCDictionary*)m_animations->objectForKey(name);
 }
 
-void AnimationComponent::addAnimation(CCObject* animation,const std::string&  name)
+/**
+ * 添加一个动画
+ */
+void AnimationComponent::addAnimation(CCAnimation* animation,const std::string&  name)
 {
 	m_animations->setObject(animation ,name);
 }
 
-void AnimationComponent::addAction(CCAction* action ,const std::string& name)
+/**
+ * 添加一个动画列表
+ */
+void AnimationComponent::addAnimationList(CCArray* animationList ,const std::string& name)
 {
-	m_animations->setObject(action ,name);
+	m_animations->setObject(animationList ,name);
 }
 
-void AnimationComponent::addActionList(CCArray* actionList ,const std::string& name)
+/**
+ * 添加一个动画字典
+ */
+void AnimationComponent::addAnimationMap(CCDictionary* animationMap ,const std::string& name)
 {
-	m_animations->setObject(actionList ,name);
+	m_animations->setObject(animationMap ,name);
 }
 
-void AnimationComponent::addActionMap(CCDictionary* actionMap ,const std::string& name)
-{
-	m_animations->setObject(actionMap ,name);
-}
-
+/**
+ * 移除一个动画
+ */
 void AnimationComponent::removeAnimationNamed(const std::string& name)
 {
 	m_animations->removeObjectForKey(name);
 }
 
+void AnimationComponent::onChangeAnimation(Message *message)
+{
+    CCLOG("AnimationComponent::handleMessage::get message %d",message->getType());
+    
+	switch(message->getType()){
+            
+		case MSG_CHANGE_ANIMATION:
+        {
+            CCDictionary* data=message->getDictionary();
+            
+            CCString* animationName=(CCString*)data->objectForKey("name");
+            int direction=((CCInteger*) data->objectForKey("direction"))->getValue();
+            CCLOG("direction:%d name:%s",direction,animationName->getCString());
+            
+            CCAnimation* animation= animationForName(animationName->getCString(),direction);
+            
+            if(animation && m_lastAnimation!=animation){
+                CCAction* action=createActionFromAnimation(animation);
+                CCNode* owner=(CCNode*) m_owner;
+                if (m_lastAction) {
+                    owner->stopAction(m_lastAction);
+                }
+                
+                owner->runAction(action);
+                setLastAction(action);
+                setLastAnimation(animation);
+            }else {
+                CCLOG("unknow animation name %s action is null",animationName->getCString());
+            }
+            break;
+        }
+	}
+}
+
+
+
+/**
+ * 从动画里创建action
+ */
+CCAction* AnimationComponent::createActionFromAnimation(CCAnimation* animation)
+{
+    CCAnimate *animate= CCAnimate::create(animation);
+    CCAction* action=CCRepeatForever::create((CCActionInterval*)CCSequence::create(animate,NULL));
+    action->setTag(kEightDirectionAction);
+    return action;
+}
+
+/**
+ * 从文件中取得8方向动画
+ * 关键帧在一张图片里。根据名子来确定方向
+ */
 CCArray* AnimationComponent::eightDirectionActionListWithFile(const char* file ,int frameCount ,CCSize frameSize ,float delay)
 {
 	CCTexture2D *texture=CCTextureCache::sharedTextureCache()->addImage(file);
 	
-	CCArray* actions=new CCArray();
-	actions->initWithCapacity(8);
+	CCArray* animations=CCArray::createWithCapacity(8);
+
 	//move action
 	for (int i=0; i<8; i++) {
 		CCArray* animFrames=new CCArray();
@@ -108,24 +191,27 @@ CCArray* AnimationComponent::eightDirectionActionListWithFile(const char* file ,
 			CCSpriteFrame *frame=CCSpriteFrame::createWithTexture(texture ,CCRectMake(j*frameSize.width, i*frameSize.height, frameSize.width, frameSize.height));
 			animFrames->addObject(frame);
 		}
+        
 		CCAnimation *animation=CCAnimation::createWithSpriteFrames(animFrames,delay);
-		CCAnimate *animate= CCAnimate::create(animation);
-		actions->addObject(CCRepeatForever::create((CCActionInterval*)CCSequence::create(animate,NULL)));
-		animFrames->release();
+        animFrames->release();
+        
+        animations->addObject(animation);
+		
 	}
-	return actions;
+	return animations;
 }
 
+/**
+ * 从目录中取得8方向动画
+ * 关键帧是一张张图片。根据名子来确定方向
+ */
 CCArray* AnimationComponent::eightDirectionActionListWithDir(const char* dir ,int frameCount ,CCSize frameSize ,float delay,const char* pathFormat)
 {
-	CCArray *actions=new CCArray();
-	actions->initWithCapacity(8);
+	CCArray* animations=CCArray::createWithCapacity(8);
 	
 	//move action
 	char str[255] = {0};
 	for (int i=0; i<8; i++) {
-		CCArray* animFrames=new CCArray();
-		animFrames->initWithCapacity(frameCount);
 		CCAnimation* animation = CCAnimation::create();
 		for (int j=0; j<frameCount; j++) {
 			sprintf(str,pathFormat,dir,i,j);//"%s/%02d%03d.png"
@@ -134,27 +220,9 @@ CCArray* AnimationComponent::eightDirectionActionListWithDir(const char* dir ,in
 		animation->setDelayPerUnit(delay);
 		animation->setRestoreOriginalFrame(true);
 		//animation.delay=delay;
-		CCAnimate *animate= CCAnimate::create(animation);
-		actions->addObject(CCRepeatForever::create((CCActionInterval*)CCSequence::create(animate,NULL)));
-		animFrames->release();
+        animations->addObject(animation);
 	}
-	return actions;
-}
-
-void AnimationComponent::changeAction(CCDictionary* pData)
-{
-	CCString* animationName=(CCString*)pData->objectForKey("name");
-    int direction=((CCInteger*) pData->objectForKey("direction"))->getValue();
-            
-    CCAction* action=(CCAction*)actionForName(animationName->getCString(),direction);
-    if(action && m_lastAction!=action){
-        CCNode* owner=(CCNode*) m_pOwner;
-        owner->stopAction(m_lastAction);
-        owner->runAction(action);
-        setLastAction(action);
-    }else {
-        CCLOG("unknow animation name %s",animationName);
-    }
+	return animations;
 }
 
 NS_CC_YHGE_END
--- yhge/Components/AnimationComponent.h
+++ yhge/Components/AnimationComponent.h
@@ -9,56 +9,119 @@ NS_CC_YHGE_BEGIN
 class AnimationComponent : public Component
 {
 public:
+    
 	AnimationComponent();
+    
 	~AnimationComponent();
+    
 	//extend from parent
-	virtual bool init();  
+	virtual bool init();
+	virtual bool init(const std::string&  file);
+    virtual bool init(CCDictionary* data);
+    
+    virtual bool registerMessages();
+    virtual void cleanupMessages();
 
-	bool init(const std::string&  file);
-    bool init(CCDictionary* data);
+    /**
+     * 按名称取得动画
+     */
+	CCAnimation* animationForName(const std::string& name);
+	CCAnimation* animationForName(const std::string& name ,int index);
+	CCAnimation* animationForName(const std::string& name ,const std::string& key);
+    
+    /**
+     * 按名称取得动画列表
+     */
+	CCArray* animationListForName(const std::string& name);
+    
+    /**
+     * 按名称取得动画字典
+     */
+	CCDictionary* animationMapForName(const std::string& name);
 
-	CCObject* animationForName(const std::string& name);
-	CCAction* actionForName(const std::string& name);
-	CCAction* actionForName(const std::string& name ,int index);
-	CCAction* actionForName(const std::string& name ,const std::string& key);
-
-	CCArray* actionListForName(const std::string& name);
-	CCDictionary* actionMapForName(const std::string& name);
-
-	void addAction(CCAction* action, const std::string& name);
-	void addActionList(CCArray* actionList ,const std::string& name);
-	void addActionMap(CCDictionary* actionMap,const std::string& name);
-	void addAnimation(CCObject* animation ,const std::string& name);
+    /**
+     * 添加一个动画
+     */
+	void addAnimation(CCAnimation* animation, const std::string& name);
+    
+    /**
+     * 添加一个动画列表
+     */
+	void addAnimationList(CCArray* animationList ,const std::string& name);
+    
+    /**
+     * 添加一个动画字典
+     */
+	void addAnimationMap(CCDictionary* animationMap,const std::string& name);
+    
+    /**
+     * 移除一个动画
+     */
 	void removeAnimationNamed(const std::string& name);
+    
+    
+    void onChangeAnimation(Message* message);
+    
+    /**
+     * 从动画里创建action
+     */
+    static CCAction* createActionFromAnimation(CCAnimation* animation);
 
+    /**
+     * 从文件中取得8方向动画
+     * 关键帧在一张图片里。根据名子来确定方向
+     */
 	static CCArray* eightDirectionActionListWithFile(const char* file ,int frameCount ,CCSize frameSize ,float delay);
-	static CCArray* eightDirectionActionListWithDir(const char* dir ,int frameCount ,CCSize frameSize ,float delay,const char* pathFormat);
-
-	void changeAction(CCDictionary* pData);
-
-	CCAction* getLastAction()
-	{
-		return m_lastAction;
-	}
-
-	void setLastAction(CCAction* action)
-	{
-		CC_SAFE_RETAIN(action);
-		CC_SAFE_RELEASE(m_lastAction);
-		m_lastAction=action;
-	}
-
-	CCAction* moveAction();
-	void moveActoin(CCAction* action);
-
-	CCArray* actionArray();
-	void actionArray(CCArray* actionArray);
-
-	CCDictionary* animations();
-	void animations(CCDictionary* animations);
-
+	
+    /**
+     * 从目录中取得8方向动画
+     * 关键帧是一张张图片。根据名子来确定方向
+     */
+    static CCArray* eightDirectionActionListWithDir(const char* dir ,int frameCount ,CCSize frameSize ,float delay,const char* pathFormat);
+    
+public:
+    
+    inline void setAnimations(CCDictionary* animations)
+    {
+        CC_SAFE_RETAIN(animations);
+        CC_SAFE_RELEASE(m_animations);
+        m_animations = animations;
+    }
+    
+    inline CCDictionary* getAnimations()
+    {
+        return m_animations;
+    }
+    
+    inline void setLastAnimation(CCAnimation* lastAnimation)
+    {
+//        CC_SAFE_RETAIN(lastAnimation);
+//        CC_SAFE_RELEASE(m_lastAnimation);
+        m_lastAnimation = lastAnimation;
+    }
+    
+    inline CCAnimation* getLastAnimation()
+    {
+        return m_lastAnimation;
+    }
+    
+    inline void setLastAction(CCAction* lastAction)
+    {
+        CC_SAFE_RETAIN(lastAction);
+        CC_SAFE_RELEASE(m_lastAction);
+        m_lastAction = lastAction;
+    }
+    
+    inline CCAction* getLastAction()
+    {
+        return m_lastAction;
+    }
+    
 protected:
+    
 	CCDictionary* m_animations;
+    
+    CCAnimation* m_lastAnimation;
     
     CCAction* m_lastAction;
 };
