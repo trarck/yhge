78037ab26bbbfdab66c7d8173ad2128ce29f3808
trarck <trarck@gmail.com>
2014-01-15 14:43:30 +0800

    攻击组件加入消息处理

--- yhge/Components/AttackComponent.cpp
+++ yhge/Components/AttackComponent.cpp
@@ -1,6 +1,6 @@
 #include "AttackComponent.h"
-#include <yhge/Message/MessageManager.h>
-
+#include <yhge/message.h>
+#include "ComponentMessageDefine.h"
 
 USING_NS_CC;
 
@@ -10,13 +10,13 @@ AttackComponent::AttackComponent()
 :m_target(NULL)
 {
     CCLOG("AttackComponent create");
-    m_sName="AttackComponent";
+    m_name="AttackComponent";
 }
 
 AttackComponent::~AttackComponent()
 {
     CCLOG("AttackComponent destroy");
-    CC_SAFE_RELEASE(m_target);
+    CC_SAFE_RELEASE_NULL(m_target);
 }
 
 bool AttackComponent::init()
@@ -25,17 +25,39 @@ bool AttackComponent::init()
     return true;
 }
 
+bool AttackComponent::registerMessages()
+{
+    CCLOG("AttackComponent::registerMessages");
+    
+    yhge::MessageManager::defaultManager()->registerReceiver(m_owner, MSG_SET_ATTACK_TARGET, NULL, message_selector(AttackComponent::onSetAttackTarget));
+    
+    yhge::MessageManager::defaultManager()->registerReceiver(m_owner, MSG_ATTACK, NULL, message_selector(AttackComponent::onAttack));
+    
+    return true;
+}
+
+void AttackComponent::cleanupMessages()
+{
+	CCLOG("AttackComponent::cleanupMessages");
+    yhge::MessageManager::defaultManager()->removeReceiver(m_owner, MSG_SET_ATTACK_TARGET);
+    yhge::MessageManager::defaultManager()->removeReceiver(m_owner, MSG_ATTACK);
+    yhge::MessageManager::defaultManager()->removeReceiver(m_owner, MSG_TARGET_DIE);
+}
+
+
 void AttackComponent::attack()
 {
-    //if(m_target){
-    //    CCLOG("AttackComponent::startAttack");
-    //    int targetHp=m_target->getHealth();
-    //    CCLOG("current target hp %d after attack %d",targetHp,targetHp-1);
-    //    m_target->setHealth(targetHp-1);
-    //}else {
-    //    CCLOG("AttackComponent::startAttack no target");
-    //}
+    if(m_target){
+        CCLOG("AttackComponent::startAttack");
+        int targetHp=m_target->getHealth();
+        CCLOG("current target hp %d after attack %d",targetHp,targetHp-1);
+        m_target->setHealth(targetHp-1);
+    }else {
+        CCLOG("AttackComponent::startAttack no target");
+    }
 }
+
+
 
 void AttackComponent::attackWithSkillId(int skillId)
 {
@@ -45,31 +67,39 @@ void AttackComponent::attackWithSkillId(int skillId)
 	//	}
 }
 
-void AttackComponent::didTargetDie()
+/**
+ * 处理攻击消息
+ */
+void AttackComponent::onAttack(Message *message)
+{
+    CCObject* target=message->getData();
+    if(target){
+        setTarget(target);
+    }
+    attack();
+}
+
+/**
+ * 处理设置目标消息
+ */
+void AttackComponent::onSetAttackTarget(Message *message)
+{
+    CCObject* target=message->getData();
+    
+    MessageManager::defaultManager()->removeReceiver(this, MSG_TARGET_DIE, m_target, message_selector(AttackComponent::onTargetDie));
+    setTarget(message->getData());
+    MessageManager::defaultManager()->registerReceiver(this, MSG_TARGET_DIE, m_target, message_selector(AttackComponent::onTargetDie));
+}
+
+/**
+ * 处理目标死亡消息
+ */
+void AttackComponent::onTargetDie(Message *message)
 {
     CCLOG("target is die");
-    
-    ////remove message
-    //MessageManager::defaultManager()->removeReceiver(this, DIE, m_target, message_selector(AttackComponent::handleMessage));
-    //CC_SAFE_RELEASE(m_target);
-    //m_target=NULL;
-}
-
-CCObject* AttackComponent::getTarget()
-{
-    return m_target;
-}
-
-void AttackComponent::setTarget(CCObject* target)
-{
-    if(target!=m_target){
-        //MessageManager::defaultManager()->removeReceiver(this, DIE, m_target, message_selector(AttackComponent::handleMessage));
-        CC_SAFE_RETAIN(target);
-        CC_SAFE_RELEASE(m_target);
-        m_target=target;
-
-        //MessageManager::defaultManager()->registerReceiver(this, message_selector(AttackComponent::handleMessage), DIE, m_target);
-    }
+    MessageManager::defaultManager()->removeReceiver(this, MSG_TARGET_DIE, m_target, message_selector(AttackComponent::onTargetDie));
+    CC_SAFE_RELEASE(m_target);
+    m_target=NULL;
 }
 
 NS_CC_YHGE_END
--- yhge/Components/AttackComponent.h
+++ yhge/Components/AttackComponent.h
@@ -1,30 +1,67 @@
-#ifndef YHGE_COMPONENTS_ATTACKCOMPONENT_H_
-#define YHGE_COMPONENTS_ATTACKCOMPONENT_H_
+#ifndef YHGE_COMPONENTS_ATTACK_COMPONENT_H_
+#define YHGE_COMPONENTS_ATTACK_COMPONENT_H_
 
 #include "cocos2d.h"
 #include "Component.h"
 
 NS_CC_YHGE_BEGIN
 
+
 class AttackComponent : public Component 
 {
 public:
+    
     AttackComponent();
     ~AttackComponent();
     virtual bool init();
-
-	virtual void attack();
+    
+    virtual bool registerMessages();
+    virtual void handleMessage(Message* message);
+    virtual void cleanupMessages();
+    
+    /**
+     * 攻击
+     */
+    virtual void attack();
+    
+    /**
+     * 使用技能攻击
+     */
     virtual void attackWithSkillId(int skillId);
+
+    /**
+     * 处理攻击消息
+     */
+    void onAttack(Message *message);
     
-    virtual void didTargetDie();
+    /**
+     * 处理设置目标消息
+     */
+    void onSetAttackTarget(Message *message);
     
-    virtual void setTarget(CCObject* target);
-    CCObject* getTarget();
+    /**
+     * 处理目标死亡消息
+     */
+    void onTargetDie(Message *message);
+public:
+    
+    inline void setTarget(CCObject* target)
+    {
+        CC_SAFE_RETAIN(target);
+        CC_SAFE_RELEASE(m_target);
+        m_target = target;
+    }
+    
+    inline CCObject* getTarget()
+    {
+        return m_target;
+    }
     
 protected:
+    
     CCObject* m_target;
 };
 
 NS_CC_YHGE_END
 
-#endif //YHGE_COMPONENTS_ATTACKCOMPONENT_H_
+#endif //YHGE_COMPONENTS_ATTACK_COMPONENT_H_
