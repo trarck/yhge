b8e57d3c7e7e7057bef302be3228aecb6686386b
trarck <trarck@gmail.com>
2014-01-15 14:27:58 +0800

    加入说明

--- yhge/message.h
+++ yhge/message.h
@@ -1,6 +1,8 @@
 #ifndef COCOS_YHGE_MESSAGE_H_
 #define COCOS_YHGE_MESSAGE_H_
 
+//建议游戏中用到的消息id从100000开始
+
 #include "Message/Message.h"
 #include "Message/MessageHandler.h"
 #include "Message/MessageManager.h"
