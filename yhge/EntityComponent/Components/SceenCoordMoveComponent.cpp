02c919fdfb5dac17cf65cd2d229e4fe536111341
trarck <trarck@gmail.com>
2014-01-28 10:01:14 +0800

    加入基于屏幕坐标移动组件

---
+++ yhge/EntityComponent/Components/SceenCoordMoveComponent.cpp
@@ -0,0 +1,591 @@
+/**
+ * 现在是按照位置来判断结束于否。
+ * 可以考虑按时间来判断。
+ */
+#include "SceenCoordMoveComponent.h"
+#include <yhge/message.h>
+#include <yhge/CocosExt/CCGeometryValue.h>
+#include "ComponentMessageDefine.h"
+#include <yhge/EntityComponent/Entity.h>
+
+USING_NS_CC;
+
+NS_CC_YHGE_BEGIN
+
+static int kDirectionMapping[8]={
+    4,1,5,
+    0,  2,
+    7,3,6
+};
+
+SceenCoordMoveComponent::SceenCoordMoveComponent()
+:Component("SceenCoordMoveComponent")
+,m_moveState(MoveIdle)
+,m_moveType(kMoveNone)
+,m_speed(0.0f)
+,m_speedX(0.0f)
+,m_speedY(0.0f)
+,m_direction(0.0f)
+,m_directionX(0)
+,m_directionY(0)
+,m_directionFlagX(0)
+,m_directionFlagY(0)
+,m_to(CCPointZero)
+,m_moving(false)
+,m_fromIndex(0)
+,m_hasEndPosition(false)
+,m_pCurrentPaths(NULL)
+,m_pNextPaths(NULL)
+,m_pathIndex(0)
+,m_update(NULL)
+{
+
+}
+
+SceenCoordMoveComponent::~SceenCoordMoveComponent()
+{
+	CC_SAFE_RELEASE(m_pCurrentPaths);
+	CC_SAFE_RELEASE(m_pNextPaths);
+}
+
+bool SceenCoordMoveComponent::init()
+{
+	if(Component::init()){
+		m_moveState=MoveStop;      
+	}
+    return true;
+}
+
+bool SceenCoordMoveComponent::init(float speed)
+{
+	if (init()) {
+		m_speed=speed;
+	}
+	return true;
+}
+
+void SceenCoordMoveComponent::setup()
+{
+    Component::setup();
+    m_isoPositionComponent=static_cast<ISOPositionComponent*>(m_owner->getComponent("ISOPositionComponent"));
+    m_rendererComponent=static_cast<RendererComponent*>(m_owner->getComponent("RendererComponent"));
+}
+
+void SceenCoordMoveComponent::cleanup()
+{
+    m_isoPositionComponent=NULL;
+    m_rendererComponent=NULL;
+    Component::cleanup();
+}
+
+bool SceenCoordMoveComponent::registerMessages()
+{
+    if(Component::registerMessages()){
+        MessageManager* messageManager=MessageManager::defaultManager();
+        
+        messageManager->registerReceiver(m_owner, MSG_MOVE_DIRECTION, NULL, message_selector(SceenCoordMoveComponent::onMoveDirection),this);
+        messageManager->registerReceiver(m_owner, MSG_MOVE_STOP, NULL, message_selector(SceenCoordMoveComponent::onMoveStop),this);
+        messageManager->registerReceiver(m_owner, MSG_MOVE_TO, NULL, message_selector(SceenCoordMoveComponent::onMoveTo),this);
+        
+        return true;
+    }
+    
+    return false;
+
+}
+
+void SceenCoordMoveComponent::cleanupMessages()
+{
+    this->getMessageManager()->removeReceiver(m_owner, MSG_MOVE_DIRECTION);
+    this->getMessageManager()->removeReceiver(m_owner, MSG_MOVE_DIRECTION_STOP);
+    this->getMessageManager()->removeReceiver(m_owner, MSG_MOVE_TO);
+    
+    Component::cleanupMessages();
+}
+
+/**
+ * 移动之前进行检查
+ */
+
+bool SceenCoordMoveComponent::checkMoveable()
+{
+	return true;
+}
+
+/**
+ * 开始移动
+ * 设置移动动画的定时器
+ */
+void SceenCoordMoveComponent::startMove()
+{
+    CCLOG("startMove");
+	m_moveState=MoveStart;
+    startMoveUpdateSchedule();
+    doMoveStart();
+}
+
+/**
+ * 停止移动
+ * 由于不是按格子移动，可以立即停止移动
+ */
+void SceenCoordMoveComponent::stopMove()
+{
+    stopMoveUpdateSchedule();
+    m_moveState=MoveStop;
+    doMoveStop();
+}
+
+void SceenCoordMoveComponent::moveTo(const CCPoint& to)
+{
+    if (m_moveState==MoveStop) {
+        
+        m_moveType=kMoveTo;
+        
+        CCPoint from=m_rendererComponent->getRenderer()->getPosition();
+        if(prepareTo(to, from)){
+            if(checkMoveable()){
+                prepareMove();
+                startMove();
+            }
+        }
+    }else{
+        continueMoveTo(to);
+    }
+}
+
+void SceenCoordMoveComponent::continueMoveTo(const CCPoint& to)
+{
+    CCAssert(m_moveType==kMoveTo, "SceenCoordMoveComponent::continueMoveTo before move type is not the same");
+    
+    CCPoint from=m_rendererComponent->getRenderer()->getPosition();
+    
+    if(prepareTo(to, from)){
+        if (checkMoveable()) {
+            prepareMove();
+        }else{
+            stopMove();
+        }
+    }else{
+        stopMove();
+    }
+}
+
+#pragma mark -
+#pragma mark 按方向移动
+
+void SceenCoordMoveComponent::moveWithDirection(float direction)
+{
+    moveWithDirection(direction,false);
+}
+
+void SceenCoordMoveComponent::moveWithDirection(float direction,bool hasTo)
+{
+    CCLOG("moveWithDirection:%f",direction);
+    
+    if (m_moveState==MoveStop) {
+        
+        m_moveType=kMoveDirection;
+        
+        prepareDirection(direction);
+
+        if(checkMoveable()){
+            prepareMove();
+            startMove();
+        }
+	}else {
+        continueMoveWithDirection(direction);
+	}
+}
+
+/**
+ * 按指定方向偏移量,这里不考虑速度叠加.
+ */
+void SceenCoordMoveComponent::moveWithDirection(float directionX ,float directionY)
+{
+    moveWithDirection(directionX, directionY, false);
+}
+
+/**
+ * 按指定方向偏移量,这里不考虑速度叠加.
+ */
+void SceenCoordMoveComponent::moveWithDirection(float directionX ,float directionY,bool hasTo)
+{
+    if (m_moveState==MoveStop) {
+        
+        m_moveType=kMoveDirection;
+        
+        prepareDirection(directionX,directionY);
+        
+        if(checkMoveable()){
+            prepareMove();
+            startMove();
+        }
+	}else {
+        continueMoveWithDirection(directionX,directionY);
+	}
+}
+/**
+ * 断续指定方向移动
+ */
+void SceenCoordMoveComponent::continueMoveWithDirection(float direction)
+{
+    CCAssert(m_moveType==kMoveDirection, "SceenCoordMoveComponent::continueMoveWithDirection before move type is not the same");
+	prepareDirection(direction);
+    if (checkMoveable()) {
+        prepareMove();
+    }else{
+        stopMove();
+    }
+}
+
+/**
+ * 断续指定方向移动
+ */
+void SceenCoordMoveComponent::continueMoveWithDirection(float directionX ,float directionY)
+{
+    CCAssert(m_moveType==kMoveDirection, "SceenCoordMoveComponent::continueMoveWithDirection before move type is not the same");
+	prepareDirection(directionX,directionY);
+    if (checkMoveable()) {
+        prepareMove();
+    }else{
+        stopMove();
+    }
+}
+
+/**
+ * 按指定路径移动
+ */
+void SceenCoordMoveComponent::moveWithPaths(CCArray* paths)
+{
+	moveWithPaths(paths ,0);
+}
+
+void SceenCoordMoveComponent::moveWithPaths(CCArray* paths,int fromIndex)
+{
+	if (m_moveState==MoveStop) {
+        
+        m_moveType=kMovePath;
+        
+		m_fromIndex=fromIndex;
+		this->setCurrentPaths(paths);
+		preparePath();
+		if (checkMoveable()) {
+            prepareMove();
+			startMove();
+		}
+	}else{
+		m_fromIndex=fromIndex;
+		continueMoveWithPaths(paths);
+	}
+}
+/**
+ * 继续指定路径移动
+ */
+void SceenCoordMoveComponent::continueMoveWithPaths(CCArray* paths)
+{
+    CCAssert(m_moveType==kMovePath, "SceenCoordMoveComponent::continueMoveWithPaths before move type is not the same");
+	this->setNextPaths(paths);
+    preparePath();
+    if (checkMoveable()) {
+        prepareMove();
+    }else{
+        stopMove();
+    }
+//	m_moveState=MoveContinue;
+}
+
+/**
+ * 路径移动之前进行检查
+ */
+bool SceenCoordMoveComponent::beforeMovePath()
+{
+	if(checkMoveable()){
+		calcDirection();
+		return true;
+	}
+	return false;
+}
+
+void SceenCoordMoveComponent::restartMoveWithPaths()
+{
+	m_moveState=MoveStart;
+	preparePath();
+	beforeMovePath();
+}
+
+/**
+ * 取得当前路径结点索引
+ */
+int SceenCoordMoveComponent::getCurrentPathIndex(){
+	return m_pCurrentPaths->count()-2-m_fromIndex;
+}
+
+/**
+ * 计算方向
+ * 主要用于按路径移动时
+ */
+void SceenCoordMoveComponent::calcDirection()
+{
+	CCPoint pos=m_rendererComponent->getRenderer()->getPosition();
+	m_directionX=m_to.x>pos.x?1:m_to.x<pos.y?-1:0;
+	m_directionY=m_to.y>pos.y?1:m_to.y<pos.y?-1:0;
+}
+
+/**
+ * 计算速度分量
+ */
+void SceenCoordMoveComponent::calcSpeedVector(){
+    m_speedX=m_speed*m_directionX;
+    m_speedY=m_speed*m_directionY;
+}
+
+/**
+ * 移动动画步骤
+ * 通过方向移动的动画步骤
+ */
+void SceenCoordMoveComponent::updateTo( float delta)
+{
+    CCNode* renderer=m_rendererComponent->getRenderer();
+    
+    CCPoint pos=renderer->getPosition();
+	//根据速度计算移动距离
+	pos.x+=delta*m_speedX;
+	pos.y+=delta*m_speedY;
+    
+    //    CCLOG("x:%f,y:%f",pos.x,pos.y);
+	//判断是否结束
+	if ((m_directionFlagX * pos.x>m_directionFlagX*m_to.x  || fabs(pos.x-m_to.x)<0.00001) &&  (m_directionFlagY*pos.y> m_directionFlagY* m_to.y|| fabs(pos.y-m_to.y)<0.00001)) {
+		pos.x=m_to.x;
+		pos.y=m_to.y;
+		stopMove();
+	}
+    
+	renderer->setPosition(pos);
+}
+
+/**
+ * 移动动画步骤
+ * 通过方向移动的动画步骤
+ */
+void SceenCoordMoveComponent::updateDirection( float delta)
+{
+    CCNode* renderer=m_rendererComponent->getRenderer();
+    
+    CCPoint pos=renderer->getPosition();
+    
+	//根据速度计算移动距离
+	pos.x+=delta*m_speedX;
+	pos.y+=delta*m_speedY;
+    
+    renderer->setPosition(pos);
+    
+    //由于没有目标点，必须手动停止。或遇障碍物会停止。
+
+}
+
+/**
+ * 移动动画步骤
+ * 通过路径移动的动画步骤
+ */
+void SceenCoordMoveComponent::updatePath(float delta)
+{
+    CCNode* renderer=m_rendererComponent->getRenderer();
+    
+    CCPoint pos=renderer->getPosition();
+	//根据速度计算移动距离
+	pos.x+=delta*m_speedX;
+	pos.y+=delta*m_speedY;
+    
+//    CCLOG("x:%f,y:%f",pos.x,pos.y);
+	//判断是否结束	
+	if ((m_directionFlagX * pos.x>m_directionFlagX*m_to.x  || fabs(pos.x-m_to.x)<0.00001) &&  (m_directionFlagY*pos.y> m_directionFlagY* m_to.y|| fabs(pos.y-m_to.y)<0.00001)) {
+		pos.x=m_to.x;
+		pos.y=m_to.y;
+
+        if (--m_pathIndex>=0 && m_moveState==MoveStart) {
+			//进行下一个格子
+            preparePath(m_pathIndex);
+            if (checkMoveable()) {
+                prepareMove();
+            }else{
+                stopMove();
+            }
+		}else {
+			//stop move
+			this->setCurrentPaths(NULL);
+			stopMove();
+		}
+	}
+    
+	renderer->setPosition(pos);
+}
+
+/**
+ * 方向改变
+ * 人物在移动时要面向不同的方向
+ */
+void SceenCoordMoveComponent::doDirectionChange()
+{
+	//根据方向来确定8方向。
+    //一种方法是根据角度值来确定8方向
+    //一种是直接根据公式来映射
+	int index=kDirectionMapping[this->hadap(m_direction)];
+
+	if (index>-1) {
+		CCDictionary* data=new CCDictionary();
+		data->setObject(CCString::create("move"), "name");
+		data->setObject(CCInteger::create(index), "direction");
+    
+		this->getMessageManager()->dispatchMessage(MSG_CHANGE_ANIMATION, this, m_owner,data);
+	}
+}
+
+/**
+ * 移动结束
+ * 由移动状态转向空闲状态
+ */
+void SceenCoordMoveComponent::doMoveStart()
+{
+    doDirectionChange();
+}
+
+/**
+ * 移动结束
+ * 由移动状态转向空闲状态
+ */
+void SceenCoordMoveComponent::doMoveStop()
+{
+	CCDictionary* data=new CCDictionary();
+    data->setObject(CCString::create("idle"), "name");
+    data->setObject(CCInteger::create(0), "direction");
+    
+    this->getMessageManager()->dispatchMessage(MSG_CHANGE_ANIMATION, NULL, m_owner,data);
+
+}
+
+//处理碰撞,由子类实现。
+//TODO:触发事件。由事件接收者执行处理逻辑，比如重新寻路。
+void SceenCoordMoveComponent::doHit(CCPoint location)
+{
+	
+}
+
+void SceenCoordMoveComponent::onMoveDirection(Message *message)
+{
+    CCInteger* integer=(CCInteger*)message->getData();
+    moveWithDirection(kmDegreesToRadians(integer->getValue()));
+}
+
+void SceenCoordMoveComponent::onMoveWithPath(Message *message)
+{
+    CCDictionary* data=message->getDictionary();
+    
+    CCArray* paths=(CCArray*)data->objectForKey("paths");
+    CCInteger* fromIndex=static_cast<CCInteger*>(data->objectForKey("fromIndex"));
+    moveWithPaths(paths,fromIndex->getValue());
+}
+
+void SceenCoordMoveComponent::onMoveTo(Message *message)
+{
+    CCPoint to=static_cast<CCPointValue*>(message->getData())->getPoint();
+    moveTo(to);
+}
+
+void SceenCoordMoveComponent::onMoveStop(Message *message)
+{
+    stopMove();
+}
+
+/**
+ * 准备移动到的数据
+ */
+bool SceenCoordMoveComponent::prepareTo(const CCPoint& to,const CCPoint& from)
+{
+    
+    float dx=to.x-from.x;
+    float dy=to.y-from.y;
+    
+    if(dx!=0 || dy!=0){
+        setTo(to);
+        float s=sqrtf(dx*dx+dy*dy);
+        prepareDirection(dx/s, dy/s);
+        return true;
+    }
+    
+    return false;
+}
+
+/**
+ * 准备方向移动的数据
+ */
+void SceenCoordMoveComponent::prepareDirection(float direction)
+{
+    m_directionX=cosf(direction);
+    m_directionY=sinf(direction);
+    m_direction=direction;
+    m_directionFlagX=m_directionX>0?1:m_directionX<0?-1:0;
+    m_directionFlagY=m_directionX>0?1:m_directionX<0?-1:0;
+}
+
+/**
+ * 准备方向移动的数据
+ */
+void SceenCoordMoveComponent::prepareDirection(float directionX,float directionY)
+{
+    m_directionX=directionX;
+    m_directionY=directionY;
+    m_direction=atan2f(directionY, directionX);
+    m_directionFlagX=m_directionX>0?1:m_directionX<0?-1:0;
+    m_directionFlagY=m_directionX>0?1:m_directionX<0?-1:0;
+}
+
+/**
+ * 准备移动路径
+ */
+void SceenCoordMoveComponent::preparePath()
+{
+	m_pathIndex=getCurrentPathIndex();
+	preparePath(m_pathIndex);
+}
+
+void SceenCoordMoveComponent::preparePath(int pathIndex)
+{
+    CCAssert(pathIndex>=0,"paths length less 2");
+	CCLOG("preparePath.PathIndex:%d",pathIndex);
+    CCPoint to= static_cast<CCPointValue*>(m_pCurrentPaths->objectAtIndex(pathIndex))->getPoint();
+    CCPoint from=m_rendererComponent->getRenderer()->getPosition();
+    prepareTo(to, from);
+}
+
+/**
+ * 移动之前的准备
+ */
+void SceenCoordMoveComponent::prepareMove()
+{
+    calcSpeedVector();
+}
+
+//开启更新定时器。为了使update不是虚函数，这里使用虚函数
+void SceenCoordMoveComponent::startMoveUpdateSchedule()
+{
+    switch (m_moveType)
+    {
+        case kMoveDirection:
+            m_update=schedule_selector(SceenCoordMoveComponent::updateDirection);
+            break;
+        case kMovePath:
+            m_update=schedule_selector(SceenCoordMoveComponent::updatePath);
+            break;
+        default:
+            break;
+    }
+    CCDirector::sharedDirector()->getScheduler()->scheduleSelector(m_update,this, 0, false);
+}
+
+void SceenCoordMoveComponent::stopMoveUpdateSchedule()
+{
+    CCDirector::sharedDirector()->getScheduler()->unscheduleSelector(m_update,this);
+    m_update=NULL;
+}
+
+NS_CC_YHGE_END
---
+++ yhge/EntityComponent/Components/SceenCoordMoveComponent.h
@@ -0,0 +1,415 @@
+#ifndef YHGE_ENTITYCOMPONENT_COMPONENTS_SCEENCOORDMOVECOMPONENT_H_
+#define YHGE_ENTITYCOMPONENT_COMPONENTS_SCEENCOORDMOVECOMPONENT_H_
+
+#include "cocos2d.h"
+#include "Component.h"
+#include "ISOPositionComponent.h"
+#include "RendererComponent.h"
+
+NS_CC_YHGE_BEGIN
+
+/**
+ * 移动组件
+ * 直接移动屏幕坐标。
+ * 万向移动.
+ * 不是按格子，如果改变方向立即生效。
+ */
+class SceenCoordMoveComponent : public Component{
+
+public:
+
+    SceenCoordMoveComponent();
+    
+    ~SceenCoordMoveComponent();
+    
+    virtual bool init();
+	bool init(float speed);
+    
+    /**
+     * 设置
+     */
+    virtual void setup();
+    
+    /**
+     * 消除
+     */
+    virtual void cleanup();
+    
+    virtual bool registerMessages();
+    virtual void cleanupMessages();
+
+    /**
+     * 检查是否可以移动
+     */
+    bool checkMoveable();
+    
+    /**
+     * 开始移动
+     */
+	void startMove();
+    
+    /**
+     * 停止移动
+     */
+    void stopMove();
+    
+    /**
+     * 移动到目标点
+     */
+	virtual void moveTo(const CCPoint& to);
+    
+    void continueMoveTo(const CCPoint& to);
+
+
+    /**
+     * 按方向移动
+     */
+    virtual void moveWithDirection(float direction);
+    
+    /**
+     * 按方向移动
+     * 是否要结束
+     */
+    virtual void moveWithDirection(float direction,bool hasTo);
+    
+    /**
+     * 按方向移动
+     * 不是方向，而是直接使用偏移量。不常用
+     */
+    virtual void moveWithDirection(float directionX,float directionY);
+    
+    /**
+     * 按方向移动
+     * 不是方向，而是直接使用偏移量。不常用
+     */
+    virtual void moveWithDirection(float directionX,float directionY,bool hasTo);
+    
+    /**
+     * 继续按方向移动
+     */
+    void continueMoveWithDirection(float direction);
+    
+    /**
+     * 继续按方向移动
+     */
+    void continueMoveWithDirection(float direction,bool hasTo);
+    
+    /**
+     * 继续按方向移动
+     * 不是方向，而是直接使用偏移量。不常用
+     */
+    void continueMoveWithDirection(float directionX,float directionY);
+    
+    /**
+     * 继续按方向移动
+     * 不是方向，而是直接使用偏移量。不常用
+     */
+    void continueMoveWithDirection(float directionX,float directionY,bool hasTo);
+    
+    /**
+     * 按路径移动
+     * 注意路径是屏幕坐标
+     */
+    virtual void moveWithPaths(CCArray* paths);
+    
+    /**
+     * 按路径移动
+     */
+	virtual void moveWithPaths(CCArray* paths, int fromIndex);
+    
+    /**
+     * 继续按路径移动
+     */
+    void continueMoveWithPaths(CCArray* paths);
+    
+    /**
+     * 继续按路径移动
+     */
+    void continueMoveWithPaths(CCArray* paths, int fromIndex);
+    
+    /**
+     * 重新按路径移动
+     */
+	void restartMoveWithPaths();
+    
+    /**
+     * 路径移动之前进行检查
+     */
+	bool beforeMovePath();
+    
+    /**
+     * 取得当前路径结点索引
+     */
+	int getCurrentPathIndex();
+    
+    /**
+     * 计算方向
+     * 主要用于按路径移动时
+     */
+	void calcDirection();
+    
+    /**
+     * 计算速度分量
+     */
+    void calcSpeedVector();
+    
+    /**
+     * 移动动画步骤
+     * 通过方向移动的动画步骤
+     */
+    void updateTo(float delta);
+    
+    /**
+     * 移动动画步骤
+     * 通过方向移动的动画步骤
+     */
+    void updateDirection(float delta);
+    
+    /**
+     * 移动动画步骤
+     * 通过路径移动的动画步骤
+     */
+	void updatePath(float delta);
+    
+    /**
+     * 移动的时候，方向改变。
+     * 主要通知人物的面向动画
+     */
+    void doDirectionChange();
+    
+    /**
+     * 开始移动的时候做一些事情。
+     */
+    void doMoveStart();
+    
+    /**
+     * 移动结束的时候做一些事情。
+     */
+    void doMoveStop();
+    
+    /**
+     * 移动被打断做一些事情。
+     */
+	void doHit(CCPoint location);
+    
+    /**
+     * 处理按方向移动消息。
+     */
+    virtual void onMoveDirection(Message* message);
+    
+    /**
+     * 处理按路径移动消息。
+     */
+    virtual void onMoveWithPath(Message* message);
+    
+    /**
+     * 处理移动到目标点。
+     */
+    virtual void onMoveTo(Message* message);
+    
+    /**
+     * 处理移动结束消息。
+     */
+    virtual void onMoveStop(Message* message);
+    
+    /**
+     * 弧度转成8方向
+     */
+    static int hadap(float ang)
+    {
+        int index = 4 + floor(ang * 1.250000 + 0.625000);
+        if (index == 0)
+        {
+            index = 8;
+        }
+        return index-1;
+    }
+    
+public:
+    
+    typedef enum  {
+        MoveIdle=0,
+        MoveStop,
+        MoveStart,
+        MoveWillStop,
+        MoveContinue
+    } MoveState;
+    
+    typedef enum  {
+        kMoveNone=0,
+        kMoveDirection,
+        kMovePath,
+        kMoveTo
+    } MoveType;
+    
+    
+    inline bool isMoving()
+    {
+        return m_moving;
+    }
+    
+    inline MoveState getMoveState()
+    {
+        return m_moveState;
+    }
+    
+    void setMoveType(MoveType moveType)
+    {
+        m_moveType = moveType;
+    }
+    
+    MoveType getMoveType()
+    {
+        return m_moveType;
+    }
+    
+    inline float getSpeed()
+    {
+        return m_speed;
+    }
+    
+    inline void setSpeed(float speed)
+    {
+        m_speed=speed;
+    }
+    
+    inline void setDirection(float direction)
+    {
+        m_direction=direction;
+    }
+    
+    inline float getDirection()
+    {
+        return m_direction;
+    }
+    
+    inline CCPoint getTo()
+    {
+        return m_to;
+    }
+    
+    inline void setTo(CCPoint to)
+    {
+        m_to=to;
+    }
+    
+    inline void setCurrentPaths(CCArray* pCurrentPaths)
+    {
+        CC_SAFE_RETAIN(pCurrentPaths);
+        CC_SAFE_RELEASE(m_pCurrentPaths);
+        m_pCurrentPaths = pCurrentPaths;
+    }
+    
+    inline CCArray* getCurrentPaths()
+    {
+        return m_pCurrentPaths;
+    }
+    
+    inline void setNextPaths(CCArray* pNextPaths)
+    {
+        CC_SAFE_RETAIN(pNextPaths);
+        CC_SAFE_RELEASE(m_pNextPaths);
+        m_pNextPaths = pNextPaths;
+    }
+    
+    inline CCArray* getNextPaths()
+    {
+        return m_pNextPaths;
+    }
+
+	inline void setPathIndex(int pathIndex)
+    {
+        m_pathIndex = pathIndex;
+    }
+    
+    inline int getPathIndex()
+    {
+        return m_pathIndex;
+    }
+    
+protected:
+    
+    /**
+     * 准备移动到的数据
+     */
+    bool prepareTo(const CCPoint& to,const CCPoint& from);
+    
+    /**
+     * 准备方向移动的数据
+     */
+    void prepareDirection(float direction);
+    
+    /**
+     * 准备方向移动的数据
+     */
+    void prepareDirection(float directionX,float directionY);
+    
+    /**
+     * 准备路径数据
+     */
+	void preparePath();
+    
+    void preparePath(int pathIndex);
+    
+    /**
+     * 移动之前的准备
+     */
+    virtual void prepareMove();
+    
+    //开启更新定时器。为了使update不是虚函数，这里使用虚函数
+    virtual void startMoveUpdateSchedule();
+    virtual void stopMoveUpdateSchedule();
+
+protected:
+    //移动状态
+	MoveState m_moveState;
+    
+    //移动类型
+    MoveType m_moveType;
+    
+	//移动速度，屏幕坐标速度
+	float m_speed;
+    
+    //速度分量
+    float m_speedX;
+    float m_speedY;
+    
+    //移动方向。屏幕坐标角度
+	float m_direction;
+    
+    //移动方向的分量
+    float m_directionX;//vector
+    float m_directionY;
+    
+//    //下个移动方向
+//    float m_nextDirection;
+
+    //移动方向标记
+    int m_directionFlagX;
+    int m_directionFlagY;
+    
+    //移动到的位置.屏幕坐标
+    CCPoint m_to;
+    
+    //是否正在移动
+	bool m_moving;
+    
+    //更新函数
+    SEL_SCHEDULE m_update;
+    
+    //是否需要检查移动结束。有时候是一直移动下去
+    bool m_hasEndPosition;
+    
+    
+	int m_fromIndex;
+	CCArray* m_pCurrentPaths;
+	CCArray* m_pNextPaths;
+	int m_pathIndex;
+    
+    ISOPositionComponent* m_isoPositionComponent;
+    RendererComponent* m_rendererComponent;
+};
+
+NS_CC_YHGE_END
+
+#endif //YHGE_ENTITYCOMPONENT_COMPONENTS_SCEENCOORDMOVECOMPONENT_H_
