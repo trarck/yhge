aa5222b93933a7cc5efe7dffecc0f4d36f62b831
trarck <trarck@gmail.com>
2014-01-28 15:42:23 +0800

    加入块大小的默认宏定义

--- yhge/Isometric/ISOStaticCoordinate.cpp
+++ yhge/Isometric/ISOStaticCoordinate.cpp
@@ -3,11 +3,19 @@
 
 NS_CC_YHGE_BEGIN
 
-float ISOStaticCoordinate::sTileWidth=64;
-float ISOStaticCoordinate::sTileHeight=32;
-float ISOStaticCoordinate::sXUnit=32;
-float ISOStaticCoordinate::sYUnit=16;
-float ISOStaticCoordinate::sZUnit=32;
+#ifndef YHGE_ISO_DEFAULT_TILEWIDTH
+#define YHGE_ISO_DEFAULT_TILEWIDTH 64
+#endif
+
+#ifndef YHGE_ISO_DEFAULT_TILEHEIGHT
+#define YHGE_ISO_DEFAULT_TILEHEIGHT 32
+#endif
+
+float ISOStaticCoordinate::sTileWidth=YHGE_ISO_DEFAULT_TILEWIDTH;
+float ISOStaticCoordinate::sTileHeight=YHGE_ISO_DEFAULT_TILEHEIGHT;
+float ISOStaticCoordinate::sXUnit=YHGE_ISO_DEFAULT_TILEWIDTH/2;
+float ISOStaticCoordinate::sYUnit=YHGE_ISO_DEFAULT_TILEHEIGHT/2;
+float ISOStaticCoordinate::sZUnit=YHGE_ISO_DEFAULT_TILEHEIGHT;
 
 
 void ISOStaticCoordinate::initTileSize(int tileWidth,int tileHeight)
