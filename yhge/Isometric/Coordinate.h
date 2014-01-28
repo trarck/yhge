4e0d00e7444351083d48f25c9eacb3b9a2857d9c
trarck <trarck@gmail.com>
2014-01-28 15:33:03 +0800

    使用新的名称。

--- yhge/Isometric/Coordinate.h
+++ yhge/Isometric/Coordinate.h
@@ -6,11 +6,6 @@
 
 NS_CC_YHGE_BEGIN
 
-typedef struct {
-	int x;
-	int y;
-} CCCell;
-
 class Coordinate : public CCObject {
 
 public:
@@ -41,54 +36,133 @@ public:
 	void setCoordinateUnit(int xUnit ,int yUnit ,int zUnit);
 	void setCoordinateUnit(int xUnit ,int yUnit);
 
-	CCPoint screenToMap(float x ,float y){
-		x=x/m_tileWidth;
-		y=y/m_tileHeight;
-		return CCPointMake(x+y,y-x);
+	//==============使用传值的方式==========================//
+	inline CCPoint isoViewToGame2F(float x,float y)
+	{
+		CCPoint p;
+		x=x/m_tileWidth;//x=x/64
+		y=y/m_tileHeight;//y=y/32
+		p.x=x+y;
+		p.y=y-x;
+		return p;
 	}
-
-	CCPoint screenToMap(const CCPoint& point){
-		return screenToMap(point.x,point.y);
+    
+	inline CCPoint isoViewToGamePoint(const CCPoint& point)
+	{
+		return isoViewToGame2F(point.x,point.y);
 	}
-
-	CCPoint screenToMapGrid(float x ,float y){
-		CCPoint p=screenToMap(x,y);
+    
+	inline CCPoint isoViewToGameGrid2F (float x,float  y)
+	{
+		CCPoint p=isoViewToGame2F(x,y);
 		p.x=floor(p.x);
 		p.y=floor(p.y);
 		return p;
 	}
-
-	CCPoint screenToMapGrid(const CCPoint& point){
-		return screenToMapGrid(point.x ,point.y);
+    
+	inline CCPoint isoViewToGameGridPoint (const CCPoint& point)
+	{
+		return isoViewToGameGrid2F(point.x,point.y);
 	}
-
-	CCCell screenToMapCell(float x ,float y){
-		CCPoint p=screenToMap(x ,y);
-		CCCell cell;
-		cell.x=(int) p.x;
-		cell.y=(int) p.y;
-		return cell;
+    
+	inline CCPoint isoViewToGameCell2F (float x,float  y)
+	{
+		CCPoint p=isoViewToGame2F(x,y);
+		p.x=(int)p.x;
+		p.y=(int)p.y;
+		return p;
 	}
-
-	CCPoint mapToScreen(float x ,float y ,float z){
+    
+    
+	inline CCPoint isoGameToView3F(float x ,float y ,float z)
+	{
 		double sx=x-y,sy=x+y;
-		return CCPointMake(sx*m_xUnit,sy*m_yUnit-z*m_zUnit);
+		CCPoint p;
+		p.x=sx*m_xUnit;//sx*32
+		p.y=sy*m_yUnit-z*m_zUnit;//sy*16-z*32
+		return p;
 	}
-
-	CCPoint mapToScreen(float x ,float y){
-		//return mapToScreen(x ,y ,0);
+    
+	inline CCPoint isoGameToView2F(float x, float y)
+	{
 		double sx=x-y,sy=x+y;
-		return CCPointMake(sx*m_xUnit,sy*m_yUnit);
+		CCPoint p;
+		p.x=sx*m_xUnit;//sx*32
+		p.y=sy*m_yUnit;//sy*16
+		return p;
+	}
+    
+	inline CCPoint isoGameToViewPoint(const CCPoint&  point)
+	{
+		return isoGameToView2F(point.x,point.y);
+	}
+    
+	//==============使用传址的方式=====================//
+    
+	inline void isoViewToGame2FP(float x,float y,CCPoint* destPoint)
+	{
+		x=x/m_tileWidth;//x=x/64
+		y=y/m_tileHeight;//y=y/32
+		destPoint->x=x+y;
+		destPoint->y=y-x;
+	}
+    
+	inline void isoViewToGamePointP(const CCPoint& point,CCPoint* destPoint)
+	{
+		isoViewToGame2FP(point.x,point.y,destPoint);
+	}
+    
+	inline void isoViewToGameGrid2FP(float x,float  y,CCPoint* destPoint)
+	{
+		isoViewToGame2FP(x,y,destPoint);
+		destPoint->x=floor(destPoint->x);
+		destPoint->y=floor(destPoint->y);
+	}
+    
+	inline void isoViewToGameGridPointP (const CCPoint& point,CCPoint* destPoint)
+	{
+		isoViewToGameGrid2FP(point.x,point.y,destPoint);
+	}
+    
+	inline void isoViewToGameCell2FP(float x,float  y,CCPoint* destPoint)
+	{
+		isoViewToGame2FP(x,y,destPoint);
+		destPoint->x=(int)destPoint->x;
+		destPoint->y=(int)destPoint->y;
+	}
+    
+    
+	inline void isoGameToView3FP(float x ,float y ,float z,CCPoint* destPoint)
+	{
+		double sx=x-y,sy=x+y;
+		destPoint->x=sx*m_xUnit;//sx*32
+		destPoint->y=sy*m_yUnit-z*m_zUnit;//sy*16-z*32
+	}
+    
+	inline void isoGameToView2FP(float x, float y,CCPoint* destPoint)
+	{
+		double sx=x-y,sy=x+y;
+		destPoint->x=sx*m_xUnit;//sx*32
+		destPoint->y=sy*m_yUnit;//sy*16
+	}
+    
+	inline void isoGameToViewPointP(const CCPoint&  point,CCPoint* destPoint)
+	{
+		isoGameToView2FP(point.x,point.y,destPoint);
 	}
 
-	CCPoint mapToScreen(const CCPoint& point){
-		return mapToScreen(point.x,point.y);
-	}
-
-	CCSize mapToScreenSize(int l ,int b ,int h);
-
-	CCPoint mapToScreenAnchor(int l ,int b ,int h);
-
+    //==============其它工具方法=====================//
+    /**
+     * 物体在地图坐标的大小转成屏幕坐标大小
+     */
+    CCSize mapToScreenSize(int l ,int b ,int h);
+    
+    /**
+     * 根据物体在地图坐标的大小计算其在显示的时候屏幕坐标的锚点
+     * 如果是opengl坐标系，则对齐点在中下，和屏幕坐标系统不同。如果是opengl，则不需要转换锚点。
+     */
+    CCPoint mapToScreenAnchor(int l ,int b ,int h);
+    
 private:
 	int m_xUnit;
 	int m_yUnit;
