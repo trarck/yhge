c5ec9b894b02f05afdc29e4761459183ef1fafe0
trarck <trarck@gmail.com>
2014-03-13 14:19:16 +0800

    修正宏定义错误

--- yhge/DataStructure/DataStructureMacros.h
+++ yhge/DataStructure/DataStructureMacros.h
@@ -2,7 +2,7 @@
 #define COCOS_YHGE_DATASTRUCTURE_DATASTRUCTUREMACROS_H_
 
 #define NS_CC_YHGE_DATASTRUCTURE_BEGIN                     namespace cocos2d { namespace yhge { namespace ds {
-#define NS_CC_YHGE_DATASTRUCTURE_END                       }}
+#define NS_CC_YHGE_DATASTRUCTURE_END                       }}}
 #define USING_NS_CC_YHGE_DATASTRUCTURE                     using namespace cocos2d::yhge::ds
 
 
