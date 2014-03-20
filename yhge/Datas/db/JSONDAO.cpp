55d3c36735dee570ebb6a086d053b0b22d763183
trarck <trarck@gmail.com>
2014-03-20 16:25:51 +0800

    实现访问sqlite的类。

--- yhge/Datas/db/JSONDAO.cpp
+++ yhge/Datas/db/JSONDAO.cpp
@@ -2,10 +2,17 @@
 
 USING_NS_CC;
 
+USING_NS_CC_YHGE_SQLITE;
+
 NS_CC_YHGE_BEGIN
+
+static const char* kSqlSeprate=",";
+static const char* kSqlFieldQuto="`";
+static const char* kSqlNullValue="null";
 
 JSONDAO::JSONDAO()
 :m_driver(NULL)
+,m_prepareFlag(":")
 {
 
 }
@@ -14,5 +21,446 @@ JSONDAO::~JSONDAO()
 {
     CC_SAFE_RELEASE_NULL(m_driver);
 }
+
+bool JSONDAO::init()
+{
+    sqlite::SqliteDriver* sqliteDriver=new sqlite::SqliteDriver();
+    
+    m_driver=new CocosSqliteDriver(sqliteDriver);
+    
+    return true;
+}
+
+bool JSONDAO::init(const std::string& dbPath,int flag)
+{
+    sqlite::SqliteDriver* sqliteDriver=new sqlite::SqliteDriver();
+    
+    m_driver=new CocosSqliteDriver(sqliteDriver);
+    
+    sqliteDriver->connect(dbPath,flag);
+    
+    return true;
+}
+
+int JSONDAO::fetchNumber(const std::string& querySql)
+{
+    Statement stmt(*(m_driver->getPtr()),querySql);
+    
+    if(stmt.execute()){
+    
+        Column col=stmt.getColumn(0);
+        
+        return col.getInt();
+    }
+    
+    return 0;
+}
+
+Json::Value JSONDAO::fetchAll(const std::string& querySql)
+{
+    Statement stmt(*(m_driver->getPtr()), querySql);
+    
+    int colCount=stmt.getColumnCount();
+    
+    Json::Value result;
+    
+    while (stmt.executeStep()) {
+
+        Json::Value record;
+        
+        for (int i=0; i<colCount; i++) {
+            setRecordValue(stmt.getColumn(i),record);
+        }
+        
+        result.append(record);
+    }
+    
+    return result;
+
+}
+
+Json::Value JSONDAO::fetchOne(const std::string& querySql)
+{
+    Statement stmt(*(m_driver->getPtr()), querySql);
+    
+    int colCount=stmt.getColumnCount();
+    
+    Json::Value result;
+
+    if (stmt.executeStep()) {
+        for (int i=0; i<colCount; i++) {
+            setRecordValue(stmt.getColumn(i),result);
+        }
+    }
+    
+    return result;
+}
+
+int JSONDAO::insert(const std::string& insertSql)
+{
+    return m_driver->getPtr()->execute(insertSql);
+}
+
+int JSONDAO::update(const std::string& updateSql)
+{
+    return m_driver->getPtr()->execute(updateSql);
+}
+
+int JSONDAO::remove(const std::string& deleteSql)
+{
+    return m_driver->getPtr()->execute(deleteSql);
+}
+
+int JSONDAO::insert(const std::string& table,const Json::Value& data)
+{
+    //生成sql
+    std::string insertSql = "INSERT INTO "+table+" ";
+    
+    insertSql+=formateToInsertPrepare(data);
+    
+    insertSql+=";";
+    
+    Statement stmt(*(m_driver->getPtr()), insertSql);
+    
+    //绑定变量
+    Json::Value::Members members=data.getMemberNames();
+    
+    for (Json::Value::Members::iterator iter=members.begin(); iter!=members.end(); ++iter) {
+        bindStatement(stmt,*iter,data[*iter]);
+    }
+    
+    return stmt.execute();
+}
+
+void JSONDAO::batchInsert(const std::string& table,const Json::Value& data)
+{
+    //准备sql
+    std::string insertSql = "INSERT INTO "+table+" ";
+    
+    insertSql+=formateToInsertPrepare(data[0u]);
+    
+    insertSql+=";";
+    
+    //只使用一个statement
+    Statement stmt(*(m_driver->getPtr()), insertSql);
+    
+    
+    int count=data.size();
+    
+    for(unsigned int i=0;i<count;++i){
+        
+        //如果不是第一个使用stmt，要重置下。
+        if (i!=0) {
+            stmt.reset();
+        }
+        
+        //分别绑定每一条记录
+        Json::Value record=data[i];
+        
+        Json::Value::Members members=record.getMemberNames();
+        
+        for (Json::Value::Members::iterator iter=members.begin(); iter!=members.end(); ++iter) {
+            bindStatement(stmt,*iter,record[*iter]);
+        }
+        stmt.execute();
+    }
+}
+
+int JSONDAO::update(const std::string& table,const Json::Value& data,const Json::Value& where)
+{
+    //生成sql
+    std::string updateSql = "UPDATE "+table+" SET ";
+    
+    updateSql+=formateToEqualPrepare(data);
+    
+    //检查有没有更新条件
+    if (where.isNull()) {
+        
+        updateSql+=";";
+        
+        Statement stmt(*(m_driver->getPtr()), updateSql);
+        
+        //绑定更新数据
+        Json::Value::Members members=data.getMemberNames();
+        
+        for (Json::Value::Members::iterator iter=members.begin(); iter!=members.end(); ++iter) {
+            bindStatement(stmt,*iter,data[*iter]);
+        }
+        
+        return stmt.execute();
+        
+    }else{
+        //生成where语句
+        Json::Value whereData;
+        
+        std::string whereSql=formateToConditionPrepare(where, whereData);
+        
+        updateSql+=" WHERE "+whereSql;
+        
+        updateSql+=";";
+        
+        Statement stmt(*(m_driver->getPtr()), updateSql);
+        
+        //由于带标志符的也占用位置，所以问号表示的不是问号中的位置，而是整个参数中的位置。所以要跳过更新数据的变量。
+        int i=1;
+        
+        //绑定更新数据
+        Json::Value::Members members=data.getMemberNames();
+   
+        for (Json::Value::Members::iterator iter=members.begin(); iter!=members.end(); ++iter) {
+            bindStatement(stmt,*iter,data[*iter]);
+            ++i;
+        }
+        
+        //绑定where数据
+        members=whereData.getMemberNames();
+        
+        for (Json::Value::Members::iterator iter=members.begin(); iter!=members.end(); ++iter) {
+            bindStatement(stmt,i++,whereData[*iter]);
+        }
+        
+        return stmt.execute();
+    }
+}
+
+int JSONDAO::remove(const std::string& table,const Json::Value& where)
+{
+    //生成sql
+    std::string deleteSql = "DELETE FROM "+table+" WHERE ";
+    
+    Json::Value whereData;
+    
+    deleteSql+=formateToConditionPrepare(where,whereData);
+    
+    deleteSql+=";";
+    
+    Statement stmt(*(m_driver->getPtr()), deleteSql);
+    
+    //绑定条件数据
+    Json::Value::Members members=whereData.getMemberNames();
+    int i=1;
+    for (Json::Value::Members::iterator iter=members.begin(); iter!=members.end(); ++iter) {
+        bindStatement(stmt,i++,whereData[*iter]);
+    }
+    
+    return stmt.execute();
+}
+
+/*
+ 
+ {
+ 
+    "a":1,
+    "b":2,
+    "c":3
+ }
+ 
+ =>
+ 
+ (`a`,`b`,`c`) values (:a,:b,:c)
+ 
+ 
+ */
+std::string JSONDAO::formateToInsertPrepare(const Json::Value& data)
+{
+    Json::Value::Members members=data.getMemberNames();
+    
+    std::string fields="(";
+    std::string values="(";
+    
+    bool isFirst=true;
+    
+    for (Json::Value::Members::iterator iter=members.begin(); iter!=members.end(); ++iter) {
+        
+        //如果不是第一项，则加入分割符
+        if (isFirst) {
+            isFirst=false;
+        }else{
+            fields+=kSqlSeprate;
+            values+=kSqlSeprate;
+        }
+        //`a`
+        fields+= kSqlFieldQuto+ *iter + kSqlFieldQuto;
+        
+        //:a
+        values+= m_prepareFlag + *iter;
+    }
+    
+    fields+=")";
+    values+=")";
+    
+    return fields + " values " + values;
+}
+
+/*
+ 
+ {
+ 
+ "a":1,
+ "b":2,
+ "c":3
+ }
+ 
+ =>
+ 
+ `a`=:a,`b`=:b,`c`=:c
+ 
+ */
+std::string JSONDAO::formateToEqualPrepare(const Json::Value& data)
+{
+    Json::Value::Members members=data.getMemberNames();
+    
+    std::string prepareStr="";
+    
+    bool isFirst=true;
+    
+    for (Json::Value::Members::iterator iter=members.begin(); iter!=members.end(); ++iter) {
+        
+        //如果不是第一项，则加入分割符
+        if (isFirst) {
+            isFirst=false;
+        }else{
+            prepareStr+=kSqlSeprate;
+        }
+        
+        //`a`=:a
+        prepareStr+= kSqlFieldQuto+ *iter + kSqlFieldQuto
+                    + "="
+                    + m_prepareFlag + *iter;
+    }
+    
+    return prepareStr;
+}
+
+/*
+ 由于条件可以嵌套，并且可能会有重复，所以使用?来绑定
+ {
+ 
+ "a":1,
+ "b":2,
+ "c":3
+ }
+ 
+ =>
+ 
+ `a`=? AND `b`=? AND `c`=?
+ 
+ {
+ 
+ "a":1,
+ 
+ "OR":{
+     "b":2,
+     "c":3
+  }
+ }
+ 
+ `a`=? AND (`b`=? OR `c`=?)
+ 
+ */
+std::string JSONDAO::formateToConditionPrepare(const Json::Value& data,Json::Value& whereData,const std::string& separator)
+{
+    Json::Value::Members members=data.getMemberNames();
+    
+    std::string prepareStr="";
+    
+    bool isFirst=true;
+    
+    for (Json::Value::Members::iterator iter=members.begin(); iter!=members.end(); ++iter) {
+        
+        if (isFirst) {
+            isFirst=false;
+        }else{
+            prepareStr+=" "+separator+" ";
+        }
+        
+        if (*iter=="AND" || *iter=="OR") {
+            
+            prepareStr+= "("+formateToConditionPrepare(data[*iter],whereData,*iter)+")";
+            
+        }else{
+            whereData.append(data[*iter]);
+            
+            //`a`=?
+            prepareStr+= kSqlFieldQuto+ *iter + kSqlFieldQuto + "= ?";
+        }
+    }
+    
+    return prepareStr;
+}
+
+void setRecordValue(const sqlite::Column& col, Json::Value& record)
+{
+    switch (col.getType()) {
+        case SQLITE_INTEGER:
+            record[col.getName()]=col.getInt();
+            break;
+        case SQLITE_FLOAT:
+            record[col.getName()]=col.getDouble();
+            break;
+        case SQLITE_BLOB:{
+            char* blobBegin=(char*)col.getBlob();
+            char* blobEnd=blobBegin+col.getBytes();
+            record[col.getName()]=Json::Value(blobBegin,blobEnd);
+            break;
+        }
+        case SQLITE_NULL:
+            record[col.getName()]=Json::Value::null;
+            break;
+        case SQLITE_TEXT:
+            record[col.getName()]=col.getText();
+            break;
+        default:
+            break;
+    }
+}
+
+void JSONDAO::bindStatement(Statement& stmt,const std::string& name,const Json::Value& val)
+{
+    switch (val.type()) {
+        case Json::intValue:
+            stmt.bind(m_prepareFlag+name, val.asInt());
+            break;
+        case Json::realValue:
+            stmt.bind(m_prepareFlag+name, val.asDouble());
+            break;
+        case Json::stringValue:
+            stmt.bind(m_prepareFlag+name, val.asString());
+            break;
+        case Json::booleanValue:
+            stmt.bind(m_prepareFlag+name, val.asBool()?1:0);
+            break;
+        case Json::nullValue:
+            stmt.bind(m_prepareFlag+name,kSqlNullValue);
+            break;
+        default:
+            break;
+    }
+}
+
+void JSONDAO::bindStatement(Statement& stmt,int index,const Json::Value& val)
+{
+    switch (val.type()) {
+        case Json::intValue:
+            stmt.bind(index, val.asInt());
+            break;
+        case Json::realValue:
+            stmt.bind(index, val.asDouble());
+            break;
+        case Json::stringValue:
+            stmt.bind(index, val.asString());
+            break;
+        case Json::booleanValue:
+            stmt.bind(index, val.asBool()?1:0);
+            break;
+        case Json::nullValue:
+            stmt.bind(index,kSqlNullValue);
+            break;
+        default:
+            break;
+    }
+}
+
+
 
 NS_CC_YHGE_END
--- yhge/Datas/db/JSONDAO.h
+++ yhge/Datas/db/JSONDAO.h
@@ -9,7 +9,7 @@
 
 NS_CC_YHGE_BEGIN
 
-typedef CCRef<sqlite::SqliteDriver*> CocosSqliteDriver;
+typedef CCRef<sqlite::SqliteDriver> CocosSqliteDriver;
 
 class JSONDAO:public CCObject
 {
@@ -23,24 +23,124 @@ public:
     
     bool init(const std::string& dbPath,int flag);
     
+    /**
+     * @brief 取得第一行第一列的值。为了方便count(*)
+     *
+     * @return int 值
+     */
     int fetchNumber(const std::string& querySql);
     
+    /**
+     * @brief 取得所有查询结果
+     *
+     * @return json的数组
+     */
     Json::Value fetchAll(const std::string& querySql);
     
+    /**
+     * @brief 取得第一条查询结果
+     *
+     * @return json的对象
+     */
     Json::Value fetchOne(const std::string& querySql);
+    
+    /**
+     * @brief 执行sql语句
+     * 通常不用取得反回值。像insert,update,delete等。
+     *
+     * @return 影响的行数
+     */
+    int execute(const std::string sql);
+    
+    /**
+     * @brief 执行插入语句
+     *
+     * @return 影响的行数
+     */
+    int insert(const std::string& insertSql);
+    
+    /**
+     * @brief 执行更新语句
+     *
+     * @return 影响的行数
+     */
+    int update(const std::string& updateSql);
+    
+    /**
+     * @brief 执行删除语句
+     *
+     * @return 影响的行数
+     */
+    int remove(const std::string& deleteSql);
 
+    /**
+     * @brief 像一个表里插入数据
+     *
+     * @return 影响的行数
+     */
     int insert(const std::string& table,const Json::Value& data);
     
-    int update(const std::string& table,const Json::Value& data);
+    /**
+     * @brief 批量像一个表里插入数据
+     * 每条数据的列相同
+     *
+     * @return 影响的行数
+     */
+    void batchInsert(const std::string& table,const Json::Value& data);
     
-    int replace(const std::string& table,const Json::Value& data);
+    /**
+     * @brief 更新一个表里数据
+     *
+     * @return 影响的行数
+     */
+    int update(const std::string& table,const Json::Value& data,const Json::Value& where);
     
+    /**
+     * @brief 删除一个表里数据
+     *
+     * @return 影响的行数
+     */
     int remove(const std::string& table,const Json::Value& where);
+    
+    
+    /**
+     * @brief 格式化数据成插入方便使用的语句
+     *
+     * @return insert的values
+     */
+    std::string formateToInsertPrepare(const Json::Value& data);
+    
+    /**
+     * @brief 格式化数据成赋值方便使用的语句
+     *
+     * @return update的set
+     */
+    std::string formateToEqualPrepare(const Json::Value& data);
+    
+    
+    /**
+     * @brief 格式化数据成条件方便使用的语句
+     *
+     * @return where的条件
+     */
+    std::string formateToConditionPrepare(const Json::Value& data,Json::Value& whereData,const std::string& separator="AND");
     
 protected:
     
-	std::string formateToPrepare(const Json::Value& data);
+    /**
+     * @brief 设置一个记录的列值
+     */
+    void setRecordValue(const sqlite::Column& col, Json::Value& record);
     
+    /**
+     * @brief 绑定一个列值
+     */
+    void bindStatement(sqlite::Statement& stmt,const std::string& name,const Json::Value& val);
+    
+    /**
+     * @brief 绑定一个列值
+     */
+    void bindStatement(sqlite::Statement& stmt,int index,const Json::Value& val);
     
 public:
     
@@ -49,10 +149,21 @@ public:
         return m_driver;
     }
     
+    inline void setPrepareFlag(const std::string& prepareFlag)
+    {
+        m_prepareFlag = prepareFlag;
+    }
+    
+    inline const std::string& getPrepareFlag()
+    {
+        return m_prepareFlag;
+    }
+    
 protected:
     
     CocosSqliteDriver* m_driver;
 
+    std::string m_prepareFlag;
 };
 
 NS_CC_YHGE_END
