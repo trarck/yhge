c9b1118627323a4254f87cc9efd740dd96218cca
trarck <trarck@gmail.com>
2014-05-29 23:15:17 +0800

    添加avatar相关

---
+++ yhge/Animation/AvatarFrame.h
@@ -0,0 +1,27 @@
+#ifndef YHGE_ANIMATION_AVATARFRAME_H_
+#define YHGE_ANIMATION_AVATARFRAME_H_
+
+#include "cocos2d.h"
+#include "../YHGEMacros.h"
+#include "AnimationInterface.h"
+#include "Frame.h"
+
+NS_CC_YHGE_BEGIN
+
+class AvatarFrame:public Frame
+{  
+public:
+
+    AvatarFrame();
+
+    ~AvatarFrame();
+
+public:
+
+protected:
+
+};
+
+NS_CC_YHGE_END
+
+#endif //YHGE_ANIMATION_AVATARFRAME_H_
